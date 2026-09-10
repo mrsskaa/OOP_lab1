5bcb088bb1ddf25da8ff212e08a064179101f9fb
mrsskaa <mariabyrtseva@yandex.ru>
2026-09-10 15:19:15 +0300

    start

---
+++ array_ops.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <cstddef>
+
+int* array_create(std::size_t size); // new int[size], +1 слот под '\0'не нужен
+
+void array_delete(int*& arr); // delete[] и обнулить указатель
+
+int* array_resize(int* arr, std::size_t size, std::size_t new_size); // новая память + копия
+
+int* array_insert(int* arr, std::size_t& size, std::size_t pos, int value); // вставка
+
+int* array_remove(int* arr, std::size_t& size, std::size_t pos); // удаление
+
+void array_print(const int* arr, std::size_t size);
+
+int* array_merge(const int* a, size_t na, const int* b, size_t nb, size_t& out_size); //cлияние двух отсортированных массивов
